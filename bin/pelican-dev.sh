acabfcafb0e8b9b484f68f4530405258af777325
kyle.pericak <kyle.pericak@breqwatr.com>
2019-10-25 08:54:24 -0400

    Added template mount to improve theme dev workflow

--- bin/pelican-dev.sh
+++ bin/pelican-dev.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+
+# Optional Theme mount for theme development. Pass the root dir of theme as arg
+template_mount=""
+if [[ $# == 1 ]]; then
+  template_dir=$1
+  echo "Mounting $template_dir to /theme"
+  template_mount="-v $template_dir:/theme"
+fi
+
 mkdir -p output
 docker pull gcr.io/kylepericak/pelican
 docker rm -f pelican >/dev/null || true
-docker run -d \
+docker run -d $template_mount \
   --name pelican \
   -v $(pwd)/content:/content \
   -v $(pwd)/output:/output \
